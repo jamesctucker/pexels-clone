a552140e1155074f792f88e918d055ddf6c7fa13
James C. Tucker <jamesctucker@protonmail.com>
2020-08-16 20:58:15 -0500

    created view for favorites

---
+++ src/views/Favorites.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="favorites">
+    <h1>My Favorite Photos</h1>
+    <section class="favorites-section">
+      <div class="image-wrapper" v-for="item in favorites" :key="item.photo.id">
+        <img class="image" :src="item.photo.src.medium" alt="photo" />
+      </div>
+    </section>
+  </div>
+</template>
+<script>
+export default {
+  name: "Favorites",
+  computed: {
+    favorites() {
+      return this.$store.state.favoritePhotos;
+    },
+  },
+};
+</script>
+<style scoped>
+.favorites-section {
+  display: flex;
+  flex-wrap: wrap;
+  align-content: flex-start;
+  justify-content: left;
+}
+
+.image-wrapper {
+  flex-grow: 1;
+  flex-basis: 150px;
+  max-width: 250px;
+  margin: 1px;
+  position: relative;
+}
+
+.image {
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+}
+</style>
