51d5e512b53de88b0c91b3b5f50315acc3abc448
James C. Tucker <jamesctucker@protonmail.com>
2020-08-16 20:58:31 -0500

    set up store to track favorited images

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,28 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    favoritePhotos: [],
   },
   mutations: {
+    addFavoritePhoto(state, photo) {
+      state.favoritePhotos.push(photo);
+    },
+    deleteFavoritePhoto(state, photo) {
+      let index = state.favoritePhotos.indexOf(photo);
+      state.favoritePhotos.splice(index);
+    },
   },
   actions: {
+    addFavorite({ commit }, photo) {
+      commit("addFavoritePhoto", photo);
+    },
+    deleteFavorite({ commit }, photo) {
+      commit("deleteFavoritePhoto", photo);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
